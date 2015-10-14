85b7e4079d508143a9b82e0b7ad057f326af8c58
Jacob McKim <mckim.jacob@gmail.com>
2015-10-13 20:33:17 -0400

    Update RequestManager.php

--- WebServer/Controllers/RequestManager.php
+++ WebServer/Controllers/RequestManager.php
@@ -11,6 +11,11 @@
  * Date Of Creation: 10 - 02 - 2015                                    *
  * ------------------------------------------------------------------- */
 
+ //===================================================================//
+ //  Includes                                                         //
+ //===================================================================//
+ require_once "IController.php";
+
 //===================================================================//
 //  NOTES & BUGS AS OF 10-02-2015                                    //
 //===================================================================//
@@ -28,114 +33,120 @@ class RequestManager Implements IController {
     /* This method is used to call out a command and give a response. */
     public static function callService ($requestData) {
 
-        // --- Variable Declarations  -------------------------------//
+      // --- Variable Declarations  -------------------------------//
 
-        /* @var $serviceResult Array Contains the command results. */
-        $serviceResult = NULL;
+      /* Whether or not the command was found.                   */
+      $commandFound = True;
 
-        // --- Main Routine -----------------------------------------//
+      /* @var $serviceResult Array Contains the command results. */
+      $serviceResult = NULL;
 
-        // Make sure the serviceID element exists if so execute.
-        if ($requestData ["serviceID"] != NULL) {
+      // --- Main Routine -----------------------------------------//
 
-            // Catch any exceptions that arise from the commands.
-            try {
+      // Make sure the serviceID element exists if so execute.
+      if ($requestData ["ServiceID"] != NULL) {
 
-                // Parse for the right command to be displayed.
-                switch ( $requestData ["ServiceID"] ) {
-                    case "AddMajorCommand" : // Creates a course.
-                        $iCommand = new AddMajorCommand ($requestData);
-                        break;
-                    case "DeleteMajorCommand" : // Signs user in and creates a session.
-                        $iCommand = new DeleteMajorCommand ($requestData);
-                        break;
-                    case "UpdateMajorCommand" : // Signs user out and destroys session.
-                        $iCommand = new UpdateMajorCommand ($requestData);
-                        break;
-                    case "UpdateMajorsCommand" : // Signs user out and destroys session.
-                        $iCommand = new UpdateMajorsCommand ($requestData);
-                        break;
-                    case "GetMajorCommand" : // Updates a users credentials.
-                        $iCommand = new GetMajorCommand ($requestData);
-                        break;
-                    case "GetMajorsCommand" : // Gets Data about the user.
-                        $iCommand = new GetMajorsCommand ($requestData);
-                        break;
-                    case "AddClassCommand" : // Configures a password change.
-                        $iCommand = new AddClassCommand ($requestData);
-                        break;
-                    case "DeleteClassCommand" : // Changes a users password.
-                        $iCommand = new DeleteClassCommand ($requestData);
-                        break;
-                    case "DeleteClassesCommand" : // Changes a users password.
-                        $iCommand = new DeleteClassesCommand ($requestData);
-                        break;
-                    case "UpdateClassCommand" : // Change the profile picture.
-                        $iCommand = new UpdateClassCommand ($requestData);
-                        break;
-                    case "GetClassCommand" : // Gets the profile picture.
-                        $iCommand = new GetClassCommand ($requestData);
-                        break;
-                    case "GetClassesCommand"
-                        $iCommand = new GetClassesCommand ($requestData);
-                        break;
-                    case "AddSelectionCommand"
-                        $iCommand = new AddSelectionCommand ($requestData);
-                        break;
-                    case "DeleteSectionCommand"
-                        $iCommand = new DeleteSectionCommand ($requestData);
-                        break;
-                    case "DeleteSectionsCommand"
-                        $iCommand = new DeleteSectionsCommand ($requestData);
-                        break;
-                    case "UpdateSectionCommand"
-                        $iCommand = new UpdateSectionCommand ($requestData);
-                        break;
-                    case "GetSectionCommand"
-                        $iCommand = new GetSectionCommand ($requestData);
-                        break;
-                    case "GetSectionsCommand"
-                        $iCommand = new GetSectionsCommand ($requestData);
-                        break;
-                    case "LoginCommand"
-                        $iCommand = new LoginCommand ($requestData);
-                        break;
-                    case "LogoutCommand"
-                        $iCommand = new LogoutCommand ($requestData);
-                        break;
-                    case "ForgotPasswordsCommand"
-                        $iCommand = new ForgotPasswordsCommand ($requestData);
-                        break;
-                    default: // Service requested not found.
-                        $serviceResult = ["response" => -1,"debug =>"
-                            ."ERROR IN CODESTORMCOMMANDER: INVALID "
-                            . "COMMAND TYPE"];
-                        break;
-                }
-                // Execute command.
-                $serviceResult = $iCommand -> executeCommand();
-            }
-
-            catch (PDOException $pdoE) {
-               $serviceResult = ["response" => -1,"debug =>"
-                    ."ERROR IN REQUESTMANAGER: error in "
-                    . "db Service :\n"+$pdoE->getMessage()];
-            }
-
+        // Parse for the right command to be displayed.
+        switch ( $requestData ["ServiceID"] ) { /*
+          case "AddMajor" : // Creates a course.
+            $iCommand = new AddMajorCommand ($requestData);
+            break;
+          case "DeleteMajor" : // Signs user in and creates a session.
+            $iCommand = new DeleteMajorCommand ($requestData);
+            break;
+          case "UpdateMajor" : // Signs user out and destroys session.
+            $iCommand = new UpdateMajorCommand ($requestData);
+            break;
+          case "UpdateMajors" : // Signs user out and destroys session.
+            $iCommand = new UpdateMajorsCommand ($requestData);
+            break;
+          case "GetMajor" : // Updates a users credentials.
+            $iCommand = new GetMajorCommand ($requestData);
+            break;
+          case "GetMajors" : // Gets Data about the user.
+            $iCommand = new GetMajorsCommand ($requestData);
+            break; */
+          case "AddCourse" : // Configures a password change.
+            $iCommand = new AddClassCommand ($requestData);
+            break;
+          case "DeleteCourse" : // Changes a users password.
+            $iCommand = new DeleteClassCommand ($requestData);
+            break;
+          /*case "DeleteCourses" : // Changes a users password.
+            $iCommand = new DeleteClassesCommand ($requestData);
+            break;*/
+          case "UpdateClass" : // Change the profile picture.
+            $iCommand = new UpdateClassCommand ($requestData);
+            break;
+          /*
+          case "GetClass" : // Gets the profile picture.
+            $iCommand = new GetClassCommand ($requestData);
+            break;
+          case "GetClasses":
+            $iCommand = new GetClassesCommand ($requestData);
+            break;
+          case "AddSelection" :
+            $iCommand = new AddSelectionCommand ($requestData);
+            break;
+          case "DeleteSection" :
+            $iCommand = new DeleteSectionCommand ($requestData);
+            break;
+          case "DeleteSections" :
+            $iCommand = new DeleteSectionsCommand ($requestData);
+            break;
+          case "UpdateSection" :
+            $iCommand = new UpdateSectionCommand ($requestData);
+            break;
+          case "GetSection" :
+            $iCommand = new GetSectionCommand ($requestData);
+            break;
+          case "GetSections" :
+            $iCommand = new GetSectionsCommand ($requestData);
+            break;
+          case "Login" :
+            $iCommand = new LoginCommand ($requestData);
+            break;
+          case "Logout" :
+            $iCommand = new LogoutCommand ($requestData);
+            break;
+          case "ForgotPasswords" :
+            $iCommand = new ForgotPasswordsCommand ($requestData);
+            break; */
+          default: // Service requested not found.
+            $commandFound = false;
         }
 
-        // Improperly formated request.
-        else
-        {
-            $serviceResult = ["response" => -1,"debug =>"
-                ."ERROR IN CODESTORMCOMMANDER: Improper "
-                . "request format."];
+        // Execute command.
+        if ($commandFound) {
+          $serviceResult = $iCommand -> executeCommand();
         }
+        else {
+          $serviceResult = new commandResult ("invalidData");
+          $serviceResult->addValuePair ("Description","Service requested not found.");
+        }
+      }
 
-        // give back the result.
-        return $serviceResult;
+      else // Improperly formated request.
+      {
+        $serviceResult = new commandResult ("invalidData");
+        $serviceResult->addValuePair ("Description","Improper request format.");
+      }
 
-    }
-    //----------------------------------------------------------------//
+      // give back the result.
+      return $serviceResult;
+
+  }
+
+  /* A method called to send back a result from the executed command on the
+  server with the results.*/
+  public static function returnResult ($resultObj) {
+      // TODO: populate this.
+
+      // TODO : Fill this in.
+
+
+  }
+
+  //----------------------------------------------------------------//
 
 }

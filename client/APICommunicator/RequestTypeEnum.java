849d08790bbc05bf2d7ebc8375831395449e2d73
Jacob McKim <mckim.jacob@gmail.com>
2015-11-08 21:48:05 -0500

    Create RequestTypeEnum.java

---
+++ client/APICommunicator/RequestTypeEnum.java
@@ -0,0 +1,15 @@
+/* --------------------------------------------------------------------*
+ * RequestTypeEnum.java                                                *
+ * --------------------------------------------------------------------*
+ *  Description - This enumeration defines the type of HTTP requests   *
+ *  the api can handle as input.                                       *
+ * --------------------------------------------------------------------*
+ * Project: Classy Scheduler Client                                    *
+ * Author : McKim A. Jacob                                             *
+ * Date Of Creation: 10 - 26 - 2015                                    *
+ * ------------------------------------------------------------------- */
+public enum RequestTypeEnum
+{
+    POST,
+    GET
+}

8ca498151880df53f7d4c6d62aea171fce32542e
Jacob McKim <mckim.jacob@gmail.com>
2015-11-08 21:48:32 -0500

    Create ServicesEnum.java

---
+++ client/APICommunicator/ServicesEnum.java
@@ -0,0 +1,20 @@
+/* --------------------------------------------------------------------*
+ * ServicesEnum.java                                                   *
+ * --------------------------------------------------------------------*
+ *  Description - This enumeration defines all the possible services   *
+ *  in which can be accessed from the API.                             *
+ * --------------------------------------------------------------------*
+ * Project: Classy Scheduler Client                                    *
+ * Author : McKim A. Jacob                                             *
+ * Date Of Creation: 10 - 26 - 2015                                    *
+ * ------------------------------------------------------------------- */
+public enum ServicesEnum
+{
+    
+    ADD_COURSE,
+    DELETE_COURSE,
+    SEARCH_COURSES,
+    UPDATE_COURSE,
+    UPDATE_SCHEDULE
+    
+}

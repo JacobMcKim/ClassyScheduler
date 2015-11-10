e572b420e456a1f5ae0d3f64ecb7e8b5c2c54b0c
Jacob McKim <mckim.jacob@gmail.com>
2015-11-09 23:13:24 -0500

    Create APIResultEnum.java

---
+++ client/APICommunicator/APIResultEnum.java
@@ -0,0 +1,19 @@
+/* --------------------------------------------------------------------*
+ * APIResultEnum.java                                                  *
+ * --------------------------------------------------------------------*
+ *  Description - This enumeration defines all the possible services   *
+ *  results that can come back from the web services API.              *
+ * --------------------------------------------------------------------*
+ * Project: Classy Scheduler Client                                    *
+ * Author : McKim A. Jacob                                             *
+ * Date Of Creation: 10 - 26 - 2015                                    *
+ * ------------------------------------------------------------------- */
+public enum APIResultEnum
+{
+    SUCCESS,
+    FAILURE,
+    INVALID_DATA,
+    SYSTEM_ERROR,
+    UNKNOWN
+    
+}

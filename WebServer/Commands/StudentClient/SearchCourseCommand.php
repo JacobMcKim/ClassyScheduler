c820391441235fabdd458e814175459e647f0cc4
JacobMcKim <mckim.jacob@gmail.com>
2015-11-12 22:33:42 -0500

    Trying this out now

--- WebServer/Commands/StudentClient/SearchCourseCommand.php
+++ WebServer/Commands/StudentClient/SearchCourseCommand.php
@@ -105,6 +105,9 @@ class SearchCourseCommand extends Command {
         /* @var $searchPhrase (String) The phrase to do the search.  */
         $searchPhrase = NULL;
 
+        /* @var $resultTable (array) The result table to return data.*/
+        $resultTable;
+
         // --- Main Routine ------------------------------------------//
 
         // Check if the request contains all necessary parameters.
@@ -132,43 +135,96 @@ class SearchCourseCommand extends Command {
             // 2. Run the statement.
             if ($this->dbAccess->executeQuery($sqlQuery,$sqlParams)) {
 
-              $sqlQuery = "SELECT c.cID, d.depName, s.sectionCode, s.sectionID, s.seats,s.seatsOpen,
-                          f.firstName,f.lastName, b.buldingName, l.classroom, t.meetDays, t.creditHours, t.startTime, t.endTime
-                          FROM course AS c
-                                 JOIN department AS d
-                                     ON c.dNum = d.dID
-                                 JOIN section AS s
-                                     ON s.courseCode = c.courseCode AND s.semesterCode = ?
-                                 JOIN faculty AS f
-                                     ON f.facultyID = s.facultyID
-                                 JOIN location AS l
-                                     ON l.locationID = s.locationID
-                                 JOIN building AS b
-                                     ON b.buildingID = l.buildingID
-                                 JOIN timeblock AS t
-                                     ON t.timeblockID = s.sectionID
-                          WHERE c.courseCode = ?";
-
-              // 3. get the results.
+              // 3. get the results and make sure we have something to return.
               $courseList = $this->dbAccess->getResults();
-              var_dump($courseList);
 
-              // 4. Per result pull all the sections related to the course.
-              foreach ($courseList as &$courseCode) {
-                echo ($courseCode);
+              if ($courseList != null) {
+                $sqlQuery = "SELECT c.*, d.depName, s.sectionCode, s.sectionID, s.seats,s.seatsOpen,
+                            f.firstName,f.lastName, b.buldingName, l.classroom, t.meetDays, t.creditHours, t.startTime, t.endTime
+                            FROM course AS c
+                                   JOIN department AS d
+                                       ON c.dNum = d.dID
+                                   JOIN section AS s
+                                       ON s.courseCode = c.courseCode AND s.semesterCode = ?
+                                   JOIN faculty AS f
+                                       ON f.facultyID = s.facultyID
+                                   JOIN location AS l
+                                       ON l.locationID = s.locationID
+                                   JOIN building AS b
+                                       ON b.buildingID = l.buildingID
+                                   JOIN timeblock AS t
+                                       ON t.timeblockID = s.sectionID
+                            WHERE c.courseCode = ?";
+
                 $sqlParams = array ($this->requestContent["semID"],$courseCode);
-                if ($this->dbAccess->executeQuery($sqlQuery,$sqlParams)) {
-                  $sectionList = $this->dbAccess->getResults();
-                  var_dump($sectionList);
+                $courseResults = array();
+
+                // 4. Per result pull all the sections related to the course.
+                foreach ($courseList as &$courseCode) {
+
+                  // Execute the search query.
+                  if ($this->dbAccess->executeQuery($sqlQuery,$sqlParams)) {
+
+                    // 5. Check and see if we have sections.
+                    $sectionList = $this->dbAccess->getResults();
+                    $sectionArray = array();
+                    if ($sectionList != null) {
+
+                      // 6. Per section populate section data.
+                      foreach($sectionList as &$resSec) {
+                         $section = array ();
+                         $section.append("sectionID" => $resSec["sectionID"]);
+                         $section.append("sectionCode" => $resSec["sectionCode"]);
+                         $section.append("profFirst" => $resSec["firstName"]);
+                         $section.append("profLast" => $resSec["lastName"]);
+                         $section.append("startTime" => $resSec["startTime"]);
+                         $section.append("endTime" => $resSec["endTime"]);
+                         $section.append("meetDays" => $resSec["meetDays"]);
+                         $section.append("building" => $resSec["buildingName"]);
+                         $section.append("room" => $resSec["classroom"]);
+                         $section.append("seats" => $resSec["seats"]);
+                         $section.append("seatsOpen" => $resSec["seatsOpen"]);
+                         $sectionArray.append($section);
+                      }
+
+                      // Populate the course data.
+                      $course.append ("department",$sectionList[0]["depName"]);
+                      $course.append ("courseID",$sectionList[0]["cID"]);
+                      $course.append ("title",$sectionList[0]["title"]);
+                      $course.append ("Description",$sectionList[0]["description"]);
+                      $course.append ("creditHours",$sectionList[0]["description"]);
+                      $course.append("sections",$sectionArray);
+                      $courseResults.append($course);
+                    } // end if check for null semester result.
+
+                  } // end if check for bad request for sections.
+                  else {
+                    $commandResult = new commandResult ("systemError");
+                    $commandResult->addValuePair ("Description","Database failure.");
+                  }
+
+                } // end of for each for course.
+
+                // Append the data back if we have something.
+                if (count($courseResults) > 0) {
+                  var_dump($commandResult);
+                  $commandResult = new commandResult ("success");
+                  $commandResult->addValuePair ("courseList",$commandResult);
                 }
                 else {
-                  $commandResult = new commandResult ("systemError");
-                  $commandResult->addValuePair ("Description","Database failure.");
+                  $commandResult = new commandResult ("failed");
+                  $commandResult->addValuePair ("Description","No classes found.");
                 }
+
+              } // end if check for null courses.
+              else {
+                $commandResult = new commandResult ("failed");
+                $commandResult->addValuePair ("Description","No classes found.");
               }
-
-              // 5. Build the response of classes.
-
+            } // end if courses exist.
+            else {
+              $commandResult = new commandResult ("systemError");
+              $commandResult->addValuePair ("Description","Database failure.");
             }
 
           }

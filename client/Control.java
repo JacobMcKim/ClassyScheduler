7360805bb4b8b256af67fcb280846a47c92d4044
trevinkr <trevinkr@mail.gvsu.edu>
2015-12-09 10:54:34 -0500

    Update Control.java

--- client/Control.java
+++ client/Control.java
@@ -1,16 +1,30 @@
 
-import javax.swing.JFrame;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Control {
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class control {
 	//--------------------------------------
 	// Creates the main program frame
 	//--------------------------------------
 	public static void main (String[] args){
 		JFrame frame = new JFrame ("Classy Scheduler");
-		frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation (JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().add(new CalendarPanel());
 		frame.setExtendedState(frame.MAXIMIZED_BOTH);
-		frame.pack();
+		frame.pack(); 
 		frame.setVisible(true);
+
 	}
 }
+
